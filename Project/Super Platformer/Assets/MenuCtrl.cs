f2ddc444d6265aeb4a6ea109b766f6096d599724
PaulLinca <paultudorlinca@gmail.com>
2020-07-20 19:00:51 +0300

    Added base project

---
+++ Project/Super Platformer/Assets/MenuCtrl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuCtrl : MonoBehaviour 
+{
+	
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+
+}
