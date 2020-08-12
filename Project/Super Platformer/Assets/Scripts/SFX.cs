04538651ff9f28d3ed7ded513bb005b0b43f1ec4
PaulLinca <paultudorlinca@gmail.com>
2020-08-12 17:39:09 +0300

    Added breakable crates

--- Project/Super Platformer/Assets/Scripts/SFX.cs
+++ Project/Super Platformer/Assets/Scripts/SFX.cs
@@ -9,4 +9,6 @@ public class SFX
     public GameObject sfx_coin_pickup; // Shown when player picks coins
     public GameObject sfx_bullet_pickup; // Shown when player picks bullet powerups
     public GameObject sfx_player_land; // Shown when player lands
+    public GameObject sfx_box_fragment_1; // Shown when player lands
+    public GameObject sfx_box_fragment_2; // Shown when player lands
 }
--- Project/Super Platformer/Assets/Scripts/SFXCtrl.cs
+++ Project/Super Platformer/Assets/Scripts/SFXCtrl.cs
@@ -29,4 +29,25 @@ public class SFXCtrl : MonoBehaviour
     {
         Instantiate(sfx.sfx_player_land, position, Quaternion.identity);
     }
+    
+    public void HandleBoxBreaking(Vector3 position)
+    {
+        Debug.Log("Cat head hit the crate");
+
+        Vector3 pos1 = position;
+        pos1.x -= 0.3f;
+        Vector3 pos2 = position;
+        pos1.x += 0.3f;
+        Vector3 pos3 = position;
+        pos1.x -= 0.3f;
+        pos1.y -= 0.3f;
+        Vector3 pos4 = position;
+        pos1.x += 0.3f;
+        pos1.y += 0.3f;
+
+        Instantiate(sfx.sfx_box_fragment_1, pos1, Quaternion.identity);
+        Instantiate(sfx.sfx_box_fragment_2, pos2, Quaternion.identity);
+        Instantiate(sfx.sfx_box_fragment_2, pos3, Quaternion.identity);
+        Instantiate(sfx.sfx_box_fragment_1, pos4, Quaternion.identity);
+    }
 }
