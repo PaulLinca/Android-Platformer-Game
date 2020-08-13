cde84e071750bc11ba33859dc5017568371dff40
PaulLinca <paultudorlinca@gmail.com>
2020-08-13 12:03:35 +0300

    Added death functionality

---
+++ Project/Super Platformer/Assets/GarbageCtrl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageCtrl : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            GameCtrl.instance.PlayerDied(other.gameObject);
+        }
+        else
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
