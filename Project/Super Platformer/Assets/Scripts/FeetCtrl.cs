babcc9c7849d4f320aad22c8c01737194d843b3c
PaulLinca <paultudorlinca@gmail.com>
2020-08-19 21:45:31 +0300

    Added moving platforms

--- Project/Super Platformer/Assets/Scripts/FeetCtrl.cs
+++ Project/Super Platformer/Assets/Scripts/FeetCtrl.cs
@@ -4,11 +4,13 @@ public class FeetCtrl : MonoBehaviour
 {
     public Transform dustParticlePosition;
     PlayerCtrl playerCtrl;
-    
+    public GameObject player;
+
     void Start() 
     {
         playerCtrl = gameObject.transform.parent.gameObject.GetComponent<PlayerCtrl>();
     }
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.gameObject.CompareTag("Ground"))
@@ -17,5 +19,23 @@ public class FeetCtrl : MonoBehaviour
 
             playerCtrl.isJumping = false;
         }
+
+        if(other.gameObject.CompareTag("Platform"))
+        {
+            SFXCtrl.instance.ShowPlayerLanding(dustParticlePosition.position);
+
+            playerCtrl.isJumping = false;
+
+            player.transform.parent = other.gameObject.transform;
+        }
     }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Platform"))
+        {
+            player.transform.parent = null;
+        }
+    }
+
 }
