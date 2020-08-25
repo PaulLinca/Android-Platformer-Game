1be10721a8d534bd4b997a5323c611ccd5e2abb2
PaulLinca <paultudorlinca@gmail.com>
2020-08-25 20:22:14 +0300

    Added coin fly animation

--- Project/Super Platformer/Assets/Scripts/CoinCtrl.cs
+++ Project/Super Platformer/Assets/Scripts/CoinCtrl.cs
@@ -9,6 +9,28 @@ public class CoinCtrl : MonoBehaviour
     }
 
     public CoinFX coinFX;
+    public float flySpeed;
+    public bool startFlying;
+
+    GameObject coinMeter;
+
+    void Start()
+    {
+        startFlying = false;
+
+        if(coinFX == CoinFX.Fly)
+        {
+            coinMeter = GameObject.Find("img_coin_count");
+        }
+    }
+
+    void Update()
+    {
+        if(startFlying)
+        {
+            transform.position = Vector3.Lerp(transform.position, coinMeter.transform.position, flySpeed);
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D other) 
     {
@@ -17,6 +39,11 @@ public class CoinCtrl : MonoBehaviour
             if(coinFX == CoinFX.Vanish)
             {
                 Destroy(gameObject);
+            }
+            else if(coinFX == CoinFX.Fly)
+            {
+                gameObject.layer = 0;
+                startFlying = true;
             }
         }
     }
