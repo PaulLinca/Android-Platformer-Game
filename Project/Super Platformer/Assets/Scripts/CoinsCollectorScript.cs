d5f626ef2a8a5dfc3b6987c684739c873a3bfa75
PaulLinca <paultudorlinca@gmail.com>
2020-08-31 09:18:52 +0300

    Moved scripts to the appropriate folder

---
+++ Project/Super Platformer/Assets/Scripts/CoinsCollectorScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsCollectorScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
