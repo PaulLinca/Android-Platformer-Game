5ea4c6a26a904c2b3ef988b79f72c411abde1e0b
PaulLinca <paultudorlinca@gmail.com>
2020-09-04 11:28:09 +0300

    Added fish enemy and spawner

---
+++ Project/Super Platformer/Assets/FIshAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FIshAI : MonoBehaviour
+{
+    public float jumpSpeed;
+
+    Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        FishJump();
+    }
+
+    void FishJump()
+    {
+        rb.AddForce(new Vector2(0, jumpSpeed));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rb.velocity.y > 0)
+        {
+            spriteRenderer.flipY = false;
+        }
+        else
+        {
+            spriteRenderer.flipY = true;
+        }
+    }
+}
