46fa52268d0b7a31de650e3e4624569d2e7426b4
PaulLinca <paultudorlinca@gmail.com>
2020-09-14 10:49:12 +0300

    Added singleton DataCtrl

---
+++ Project/Super Platformer/Assets/Scripts/Controllers/DataCtrl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataCtrl : MonoBehaviour
+{
+    public static DataCtrl instance = null;
+
+    void Awake() 
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
